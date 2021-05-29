8dc533fc8d4d4fddd1dd3e3f76732be24a54ca6f
iriwen <15669923501@139.com>
2021-05-30 00:06:59 +0800

    http server response

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const http = require('http');
-const server = http.createServer((request,response)=>{
-    let data ='' ;
-    request.on('data',function(chunk){
-        data+=chunk;
+const http = require("http");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+const server = http.createServer((request, response) => {
+    let data = "";
+    request.on("data", function (chunk) {
+        data += chunk;
         console.log(data);
-    }) ;
-
-    request.on('end',()=>{
-
-
     });
 
+    request.on("end", () => {
+        var method = request.method;
+        var headers = JSON.stringify(request.headers);
+        var httpVersion = request.httpVersion; 
+
+        var url = request.url;
+        response.writeHead(200, {"Content-Type":"text/html"});
+        let responseData =
+            method + "[d]" + headers + "[d]" + httpVersion + "[d]" + url;
+        response.end(responseData);
+    });
 });
-server.listen(3333,function(){
-  console.log("server started on 3333!") ;
+
+server.listen(3333, () => {
+    console.log("server started on 3333!");
+});
+
+server.on("connection", () => {
+    console.log("client connected !");
+});
+
+server.on("close", () => {
+    console.log("server closed !");
 });
