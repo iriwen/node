36eb2d1b4808091f2992ad9a8d87de327ff6d7d8
iriwen <15669923501@139.com>
2021-05-31 22:14:44 +0800

    url模块 http 客户端

---
+++ net/urlParser.js
@@ -0,0 +1,13 @@
+const url = require("url");
+const urlStr = 'www.google.com?key=youtube';
+
+const result = url.parse(urlStr);
+console.log(result);
+
+var urlObj = {
+    protocol:"https",
+    slashes:true,
+    hostname:"www.qihu.com",
+    pathname:"/teach/content.html"
+};
+console.log(url.format(urlObj));
