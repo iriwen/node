58b76331895cea7f6a7cf83db5ab6a2261716a9f
iriwen <15669923501@139.com>
2021-05-31 23:34:01 +0800

    module的两种导出方式，util，path，dns

---
+++ modules/mymodule.js
@@ -0,0 +1,12 @@
+var info ={"name" :"lisi","sex": "woman"};
+
+
+var operation = function(number){
+    return number + 5 ;
+}
+
+exports.info = info;
+
+exports.myOperation = operation;
+
+//module.exports =testModule;
